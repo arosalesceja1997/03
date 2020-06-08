d7bd3b1a84429c3f60ad927a2ae1e515cbf0e602
Anthony Tonatihu Rosales Ceja <arosalesceja1997@gmail.com>
2020-06-08 14:22:06 -0500

    master - Canvios

---
+++ 03.rb
@@ -0,0 +1,27 @@
+class GameLive
+  def initialize()
+  end
+
+  def dibujar()
+    matrix = Array.new(3) { Array.new(4) { 0 } }
+    matrix[0][0] = 10;
+    x = matrix[0][0]
+    # print("#{x}   #{x}   #{x} a")
+    # for i in 0...10
+    #   for j in 0...10
+    #     print("matrix[#{i}][#{j}]: ")
+    #     matrix[i][j] = gets.to_i
+    #   end
+    # end
+
+    for i in 0...10
+      for j in 0...10
+        
+      end
+    end
+  end
+end
+
+obj = GameLive.new()
+obj.dibujar()
+
