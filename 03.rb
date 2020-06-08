3787c9d56c3a7bc86126c7d6de811117a073da41
Anthony Tonatihu Rosales Ceja <arosalesceja1997@gmail.com>
2020-06-08 17:25:52 -0500

    master - Canvios

--- 03.rb
+++ 03.rb
@@ -3,9 +3,8 @@ class GameLive
   end
 
   def dibujar()
-    matrix = Array.new(3) { Array.new(4) { 0 } }
-    matrix[0][0] = 10;
-    x = matrix[0][0]
+    matrix = Array.new(10) { Array.new(10) { 0 } }
+    # x = matrix[0][0]
     # print("#{x}   #{x}   #{x} a")
     # for i in 0...10
     #   for j in 0...10
@@ -13,11 +12,25 @@ class GameLive
     #     matrix[i][j] = gets.to_i
     #   end
     # end
+    ij = 0
+    # for i in 0...10
+    #   for j in 0...11
+    #     if ij <= 10
+    #       print("#{matrix[i][j]}\t")
+    #     else
+    #       ij = 0
+    #       puts
+    #     end
+    #     ij += 1;
+    #   end
+    # end
 
-    for i in 0...10
-      for j in 0...10
-        
+    matrix.each do |column|
+      column.each do |card|
+        print card
+        print ' ' # print no imprime un salto de linea al final del output
       end
+      puts '' # salto de linea
     end
   end
 end
