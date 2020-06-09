9baee4d2ac549a484e15b4f8f19b65c9421d5cf6
Anthony Tonatihu Rosales Ceja <62262756+arosalesceja1997@users.noreply.github.com>
2020-06-09 13:01:10 -0500

    Update 03.rb

--- 03.rb
+++ 03.rb
@@ -1,31 +1,10 @@
 class GameLive
   def initialize()
+    @matrix = Array.new(10) { Array.new(10) { 0 } }
   end
 
   def dibujar()
-    matrix = Array.new(10) { Array.new(10) { 0 } }
-    # x = matrix[0][0]
-    # print("#{x}   #{x}   #{x} a")
-    # for i in 0...10
-    #   for j in 0...10
-    #     print("matrix[#{i}][#{j}]: ")
-    #     matrix[i][j] = gets.to_i
-    #   end
-    # end
-    ij = 0
-    # for i in 0...10
-    #   for j in 0...11
-    #     if ij <= 10
-    #       print("#{matrix[i][j]}\t")
-    #     else
-    #       ij = 0
-    #       puts
-    #     end
-    #     ij += 1;
-    #   end
-    # end
-
-    matrix.each do |column|
+    @matrix.each do |column|
       column.each do |card|
         print card
         print ' ' # print no imprime un salto de linea al final del output
